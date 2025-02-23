791a86fdeae21bd65cb574cbde633953d601ec4c
skllzrmy.tez <joe@poundfit.com>
2025-02-22 16:58:29 -0800

    ⬆️ (manifest.json, popup.js): bump version from 1.1.0 to 1.1.2 to reflect new updates and changes in the extension

--- popup.js
+++ popup.js
@@ -107,5 +107,5 @@ function showMessage(text, color) {
 /**
  * Displays the current version of the extension.
  */
-const CURRENT_VERSION = "1.1.0";
+const CURRENT_VERSION = "1.1.2";
 document.getElementById("currentVersion").appendChild(document.createTextNode(CURRENT_VERSION));
